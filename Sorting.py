f12384c1bfaf34b9d878fd6f8cab1687c83c7b40
Yahia Elshafei <es.yehia.farid2023@alexu.edu.eg>
2026-02-26 22:54:55 +0200

    implemented insertion sort

--- Sorting.py
+++ Sorting.py
@@ -22,7 +22,17 @@ def SelectionSort(arr):
     return (time.time() - startTime) * 1000
 
 def InsertionSort(arr):
-    pass
+    startTime = time.time()
+    n = len(arr)
+
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return (time.time() - startTime) * 1000
 
 
 def MergeSortTimer(arr):
